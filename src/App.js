8c694e419f0e359f95e89ce4dc04ce3e5b29c0ea
Karan Rathod <karanrathod1184@gmail.com>
2024-04-26 15:06:20 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState("Guest");
+	const [input, setInput] = useState("");
+
+	return (
+		<div className="App">
+			<h1 style={{marginLeft: "10px"}}>Hello, {name}</h1>
+			<div>
+				<TextField
+					id="outlined-basic"
+					label="Please Enter Your Name"
+					variant="outlined"
+					style={{ width: "20%", fontSize: "15px", margin: "10px" }}
+					onChange={(e) => setInput(e.target.value)}
+					value={input}
+				/>
+			</div>
+			<Button
+				variant="contained"
+				color="success"
+				onClick={() => setName(input)}
+        style={{margin: "10px"}}
+			>
+				Submit
+			</Button>
+		</div>
+	);
 }
 
 export default App;
